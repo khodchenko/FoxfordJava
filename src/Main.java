bdcb76ede79e9caa91df3c38be3738edd161337a
khodchenko <khodch3nk0@gmail.com>
2023-03-25 03:41:29 +0100

    task2 complete

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Task2 task2 = new Task2();
+        Scanner scanner = new Scanner(System.in);
+        double[] n = new double[] {1};
 
-        System.out.println(Arrays.toString(task2.sort3Digit(2, 3, 1)));
+        System.out.println(Arrays.toString(task2.tribonacci(n, 10)));
     }
 }
--- src/Task2/Task2.java
+++ src/Task2/Task2.java
@@ -1,5 +1,10 @@
 package Task2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public double countChange(int digitA, int digitB, int digitC, int digitD) {
         double cost = digitB;
@@ -33,6 +38,55 @@ public class Task2 {
         return new int[]{digitA, digitB, digitC};
     }
 
+    public boolean checkTwoCoordinates(int x1, int y1, int x2, int y2) {
+        return x1 * x2 > 0 && y1 * y2 > 0;
+    }
+
+    public int largestNumber(Scanner scanner) {
+        int largestInt = 0;
+        List<Integer> list = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            list.add(scanner.nextInt());
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > largestInt) {
+                largestInt = list.get(i);
+            }
+        }
+        return largestInt;
+    }
+
+    public boolean triangleCheck(int a, int b, int c) {
+        return a + b > c | a + c > b | b + c > a;
+    }
+
+    public String maskify(String str) { //Credit Card Mask
+        char[] array = str.toCharArray();
+        if (array.length <= 4) {
+            return str;
+        }
+        for (int i = 0; i < array.length - 4; i++) {
+            array[i] = '#';
+        }
+        StringBuilder newString = new StringBuilder();
+        for (char c : array) {
+            newString.append(c);
+        }
+        return String.valueOf(newString);
+    }
+
+    public String maskifyShort(String str) {//Credit Card Mask
+        return str.replaceAll(".(?=.{4})", "#");
+    }
+
+    public double[] tribonacci(double[] s, int n) { //Tribonacci Sequence
+        double[] tritab = Arrays.copyOf(s, n);
+        for(int i=3;i<n;i++){
+            tritab[i]=tritab[i-1]+tritab[i-2]+tritab[i-3];
+        }
+        return tritab;
+
+    }
     public int[] swap(int a, int b) {
         int temp = a;
         a = b;
